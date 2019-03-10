da7b51126555f298b6d827be5d3675bfe69908ce
Balasubramanya <balasubramanya.s.1992@gmail.com>
2019-03-10 22:43:17 +0530

    Override Annotation

--- src/com/balasubramanya/oops/inheritance/ChildClass.java
+++ src/com/balasubramanya/oops/inheritance/ChildClass.java
@@ -5,6 +5,7 @@ package com.balasubramanya.oops.inheritance;
 
 public class ChildClass extends ParentClass{
 	
+	@Override
 	public void execute() {
 		System.out.println("Hey I'm Child of ParentClass");
 	}
--- src/com/balasubramanya/oops/polymorphism/Example2.java
+++ src/com/balasubramanya/oops/polymorphism/Example2.java
@@ -2,6 +2,7 @@ package com.balasubramanya.oops.polymorphism;
 
 public class Example2 extends Example1{
 	
+	@Override
 	public void display() {
 		System.out.println("Displayed Example2");
 	}
